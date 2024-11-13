06dfe711e387fc592fd22899cdda0e83ecf66971
Lennart Voorgang <lennart@voorgang.eu>
2024-11-13 13:25:34 +0100

    Multi-threaded metropolis

--- 4 - The Ising Model in 1D/src/lattice_measurement.cpp
+++ 4 - The Ising Model in 1D/src/lattice_measurement.cpp
@@ -1,4 +1,4 @@
 #include "lattice_measurement.h"
 
-LatticeMeasurement::LatticeMeasurement(const std::size_t lattice_size, const Measurement<int64_t> action, const Measurement<int64_t> diff_action) : lattice_size(lattice_size), action(action), diff_action(diff_action)
+LatticeMeasurement::LatticeMeasurement(const std::size_t lattice_size, const Experiment<int64_t> action, const Experiment<int64_t> diff_action) : lattice_size(lattice_size), action(action), diff_action(diff_action)
 { }
