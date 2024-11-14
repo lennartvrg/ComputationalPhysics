5cb47467342533cc7bb4137472ffa559cab43471
Lennart <7083261+lennartvrg@users.noreply.github.com>
2024-11-14 22:51:40 +0100

    Optimized simple sampling and added common library (#1)
    * Added histogram class
    
    * Fix CMake version
    
    * Backwards compatible
    
    * Added algorithm include

--- 2 - Simple Sampling Experiments/src/main.cpp
+++ 2 - Simple Sampling Experiments/src/main.cpp
@@ -1,17 +1,15 @@
 #include <cmath>
 #include <algorithm>
-#include <array>
 #include <cstddef>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <iterator>
 #include <numeric>
 #include <random>
 #include <ranges>
-#include <span>
 #include <string>
 #include <execution>
+#include "histogram.h"
 
 /**
  * The Mersenne Twister 19937 uniform random number generator with random seed.
@@ -21,7 +19,7 @@ static thread_local std::mt19937 generator {std::random_device()()};
 /**
  * A uniform distribution [0, 1] from which the generate can sample.
  */
-static std::uniform_int_distribution<int> uniform_distribution {0, 1};
+static std::uniform_int_distribution uniform_distribution {0, 1};
 
 /**
  * The iterator on the integer interval [0, 32) used for the number of coinflips
@@ -32,15 +30,29 @@ static std::ranges::iota_view flips {0, 32};
  * Copies the given span of double precision numbers into the output
  * CSV file with the given name.
  *
- * @param numbers The span of double precision numbers.
+ * @param histogram The histogram object
  * @param name The name of the output file.
  */
-void write_output(const std::span<double> numbers, const std::string & name) {
+void write_output(const histogram::Histogram & histogram, const std::string & name) {
     std::ofstream output;
     output.open("output/" + name + ".csv");
 
-    std::ranges::copy(numbers.begin(), numbers.end(), std::ostream_iterator<double>(output, "\n"));
+    output << histogram << std::endl;
     output.close();
+}
+
+/**
+ * Generates a range of size_t indices on the interval [0, S). Used to replace
+ * for loops with std::for_each with parallel execution policy.
+ *
+ * @tparam S The size of the sequence
+ * @return The range [0, S) of the sequence
+ */
+template <std::size_t S>
+std::vector<std::size_t> sequence() {
+    std::vector<std::size_t> sequence (S);
+    std::iota(sequence.begin(), sequence.end(), 0);
+    return sequence;
 }
 
 /**
@@ -65,11 +77,14 @@ template <std::size_t S>
 void sequence_of_uniform_reals()
 {
     std::cout << "\t S is " << S << std::endl;
+    histogram::Histogram histogram { 100 };
 
-    std::array<double, S> numbers {};
-    std::generate(std::execution::par_unseq, numbers.begin(), numbers.end(), uniform_real);
+    static std::vector<std::size_t> numbers = sequence<S>();
+    std::for_each(std::execution::par_unseq, numbers.begin(), numbers.end(), [&] ([[maybe_unused]] const std::size_t _) {
+        histogram.add(uniform_real());
+    });
 
-    write_output(numbers, "sequence" + std::to_string(S));
+    write_output(histogram, "sequence" + std::to_string(S));
 }
 
 /**
@@ -97,19 +112,20 @@ template <std::size_t S>
 void sequence_of_biased_reals(const double lambda)
 {
     std::cout << "\t Lambda is " << lambda << std::endl;
+    histogram::Histogram histogram { 100 };
 
     std::array<std::discrete_distribution<>, 32> distributions {};
-    std::transform(flips.begin(), flips.end(), distributions.begin(), [&] (const int j) {
+    std::transform(flips.begin(), flips.end(), distributions.begin(), [=] (const int j) {
         double p = 1.0 / (1.0 + exp(-lambda / pow(2, j + 1)));
         return std::discrete_distribution({p, 1.0 - p});
     });
 
-    std::array<double, S> numbers {};
-    std::generate(std::execution::par_unseq, numbers.begin(), numbers.end(), [&] {
-        return biased_real(distributions);
+    static std::vector<std::size_t> numbers = sequence<S>();
+    std::for_each(std::execution::par_unseq, numbers.begin(), numbers.end(), [&] ([[maybe_unused]] const std::size_t _) {
+       histogram.add(biased_real(distributions));
     });
 
-    write_output(numbers, "sequence_biased" + std::to_string((int) std::round(lambda * 10)));
+    write_output(histogram, "sequence_biased" + std::to_string(static_cast<int>(lambda * 10)));
 }
 
 /**
---
+++ Libraries/includes/histogram.h
@@ -0,0 +1,27 @@
+#ifndef HISTOGRAM_H
+#define HISTOGRAM_H
+
+#include <vector>
+#include <atomic>
+#include <cstddef>
+#include <iostream>
+#include <algorithm>
+
+namespace histogram
+{
+    class Histogram
+    {
+    public:
+        explicit Histogram(size_t bins);
+
+        void add(double value);
+        friend std::ostream & operator<<(std::ostream & os, const Histogram & histogram);
+
+    private:
+        std::vector<std::atomic<int32_t>> data;
+    };
+
+    std::ostream & operator<<(std::ostream & os, const Histogram & histogram);
+}
+
+#endif //HISTOGRAM_H
