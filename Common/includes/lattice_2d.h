c599711c4764eaa746c3aa4e22818ff0a6d42375
Lennart Voorgang <lennart@voorgang.eu>
2024-11-23 00:48:23 +0100

    2D Lattice now uses 1D vector because of cache locality

--- Common/includes/lattice_2d.h
+++ Common/includes/lattice_2d.h
@@ -6,11 +6,9 @@
 
 class Lattice2D final : public Lattice {
 public:
-	Lattice2D(const size_t lattice_length, const double beta, const double j, const double h) : Lattice(beta, j, h), spins(lattice_length, std::vector<short>(lattice_length, 1)) {
+	Lattice2D(const size_t lattice_length, const double beta, const double j, const double h) : Lattice(beta, j, h), lattice_length(lattice_length), spins(lattice_length * lattice_length, 1) {
 	    current = LatticeObservable(0, j, energy(), magnetization());
 	}
-
-	[[nodiscard]] constexpr size_t lattice_length() const;
 
 	void flip_spin(size_t i) override;
 	[[nodiscard]] constexpr size_t num_sites() const noexcept override;
@@ -24,7 +22,8 @@ public:
 	static double magnetization_diff(short old_spin);
 
 private:
-	std::vector<std::vector<short>> spins;
+	const size_t lattice_length;
+	std::vector<short> spins;
 };
 
 #endif //LATTICE_2D_H
--- Common/src/lattice_2d.cpp
+++ Common/src/lattice_2d.cpp
@@ -3,58 +3,48 @@
 #include <cstdlib>
 #include <ranges>
 
-constexpr size_t Lattice2D::lattice_length() const {
-	return spins.size();
-}
-
 void Lattice2D::flip_spin(const size_t i) {
-	const auto [quot, rem] = std::div(static_cast<int>(i), static_cast<int>(lattice_length()));
-	spins.at(quot).at(rem) *= -1;
+	const auto [col, row] = std::div(static_cast<int>(i), static_cast<int>(lattice_length));
+	spins.at(row * lattice_length + col) *= -1;
 }
 
 constexpr size_t Lattice2D::num_sites() const noexcept {
-	return lattice_length() * lattice_length();
+	return spins.size();
 }
 
 double Lattice2D::energy() const {
 	int energy = 0;
-	for (const size_t i: std::views::iota(static_cast<size_t>(0), num_sites())) {
-		const size_t length = lattice_length();
-		const auto [quot, rem] = std::div(static_cast<int>(i), static_cast<int>(length));
-
-		energy += spins.at(quot).at(rem) * (
-				spins.at((quot + 1) % length).at(rem) +
-				spins.at(quot).at((rem + 1) % length)
+	for (const size_t i : std::views::iota(static_cast<size_t>(0), num_sites())) {
+		const auto [col, row] = std::div(static_cast<int>(i), static_cast<int>(lattice_length));
+		energy += spins.at(row * lattice_length + col) * (
+				spins.at(row * lattice_length + (col + 1) % lattice_length) +
+				spins.at((row + 1) % lattice_length * lattice_length + col)
 			);
 	}
 	return -j * energy;
 }
 
 double Lattice2D::energy_diff(const size_t i) const {
-	const size_t length = lattice_length();
-	const auto [quot, rem] = std::div(static_cast<int>(i), static_cast<int>(length));
-
-	return 2 * j * spins.at(quot).at(rem) * (
-		       spins.at((quot + 1) % length).at(rem) +
-		       spins.at((quot + length - 1) % length).at(rem) +
-		       spins.at(quot).at((rem + 1) % length) +
-		       spins.at(quot).at((rem + length - 1) % length)
+	const auto [col, row] = std::div(static_cast<int>(i), static_cast<int>(lattice_length));
+	return 2 * j * spins.at(row * lattice_length + col) * (
+		       spins.at(row * lattice_length + (col + 1) % lattice_length) +
+		       spins.at(row * lattice_length + (col + lattice_length - 1) % lattice_length) +
+		       spins.at((row + 1) % lattice_length * lattice_length + col) +
+		       spins.at((row + lattice_length - 1) % lattice_length * lattice_length + col)
 	       );
 }
 
 double Lattice2D::magnetization() const {
 	int magnetization = 0;
-	for (const std::vector<short> & row: spins) {
-		for (const short spin: row) {
-			magnetization += spin;
-		}
+	for (const short spin : spins) {
+		magnetization += spin;
 	}
 	return magnetization;
 }
 
 double Lattice2D::magnetization_diff(const size_t i) const {
-	const auto [quot, rem] = std::div(static_cast<int>(i), static_cast<int>(lattice_length()));
-	return magnetization_diff(spins.at(quot).at(rem));
+	const auto [col, row] = std::div(static_cast<int>(i), static_cast<int>(lattice_length));
+	return magnetization_diff(spins.at(row * lattice_length + col));
 }
 
 double Lattice2D::magnetization_diff(const short old_spin) {
